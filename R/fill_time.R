ba588e6a544cff337ca30286e1b5e2e07975d09d
mrubayet <rubayet.ce@gmail.com>
2016-11-10 01:44:43 -0800

    Update fill_time.R

--- R/fill_time.R
+++ R/fill_time.R
@@ -3,6 +3,7 @@
 #' Thsi function fills the missing timestamp that are not available in the data
 #' due to "no precipitation"
 #' @param df (data.frame) data.frame with missing timestamp
+#' @export
 
 fill_time<-function(df){
   ts<-suppressWarnings(seq.POSIXt(as.POSIXct(min(df$Timestamp),'%Y-%m-%d %H:%M'), as.POSIXct(max(df$Timestamp),'%m/%d/%y %H:%M'), by="hour"))
